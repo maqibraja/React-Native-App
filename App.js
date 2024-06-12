dc6a65f384bdcacfa9223f9558bcad97b78216d7
Aqib Raja <maqibraja@hotmail.com>
2024-06-12 07:16:25 -0400

    design assest

--- App.js
+++ App.js
@@ -1,19 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StatusBar, StyleSheet, Text, View, ImageBackground } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ImageBackground source={require('./assets/design.jpg')} style={styles.background}>
+      <View style={styles.container}>
+        <Text>Hello Aqib Raja</Text>
+        <StatusBar style="auto" />
+      </View>
+    </ImageBackground>
   );
 }
-
 const styles = StyleSheet.create({
+  background: {
+    flex: 1,
+    resizeMode: "cover",
+    justifyContent: "center"
+  },
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'rgba(255,255,255,0.5)', // Optional: Add a semi-transparent white background
     alignItems: 'center',
     justifyContent: 'center',
   },
